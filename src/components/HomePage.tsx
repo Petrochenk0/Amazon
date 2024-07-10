7d9181fbd599083ea5b7c94065dae71319a2d7a6
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2024-07-10 13:19:43 +0300

    adding grid adaptive

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -8,6 +8,12 @@ export default function HomePage() {
       <div className="max-w-[1500px] min-w-[1000px] bg-red-400 m-auto">
         HomePage
         <Carousel />
+        <div className="grid grid-cols-3 xl:grid-cols-4">
+          <div>first product</div>
+          <div>seoncd product</div>
+          <div>third product</div>
+          <div>forth product</div>
+        </div>
       </div>
     </div>
   );
